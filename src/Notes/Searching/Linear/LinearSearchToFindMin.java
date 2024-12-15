772a4ba31be53c0246960852700e9f582bb4daba
Kevindua26 <kevindua26@gmail.com>
2024-12-15 23:16:06 +0530

    Update LinearSearchFindMin

--- src/Notes/Searching/Linear/LinearSearchToFindMin.java
+++ src/Notes/Searching/Linear/LinearSearchToFindMin.java
@@ -4,7 +4,9 @@ public class LinearSearchToFindMin {
     public static void main(String[] args) {
         int arr[] = {14, 53, 66, 32, 65, 2, 67};
 
-        if (arr.length != 0) {
+        if (arr.length == 0) {
+            System.out.println("Array is empty");
+        } else {
             System.out.println("Minimum: " + FindMin(arr));
         }
 
