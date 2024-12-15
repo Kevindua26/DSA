1d90d7d8465d515b51410ca575000be999fb5025
Kevindua26 <kevindua26@gmail.com>
2024-12-15 21:17:07 +0530

    Update LinearSearchInt

--- src/Notes/Searching/Linear/LinearSearchInt.java
+++ src/Notes/Searching/Linear/LinearSearchInt.java
@@ -1,6 +1,6 @@
 package Notes.Searching.Linear;
 
-public class LinearSearch {
+public class LinearSearchInt {
     public static void main(String[] args) {
         int arr[] = {14, 53, 66, 32, 65, 24, 67};
         int target = 65;
@@ -8,6 +8,7 @@ public class LinearSearch {
         System.out.println("The location of the element is " + location);
     }
 
+    // Using For Loop
     static int Search(int arr[], int target) {
         // If array is empty
         if (arr.length == 0) {
@@ -23,5 +24,31 @@ public class LinearSearch {
 
         // If the array doesn't contain the number (target)
         return -1;
+    }
+
+    // Using For Each Loop
+    static int Search2(int arr[], int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+        return -1;
+    }
+
+    // Using True & False
+    static boolean Search3(int arr[], int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        return false;
     }
 }
