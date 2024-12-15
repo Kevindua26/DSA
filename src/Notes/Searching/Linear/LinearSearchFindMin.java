acf9ff9d9ee2f90ba04f61b774aeb3ff51b42bb3
Kevindua26 <kevindua26@gmail.com>
2024-12-15 23:02:28 +0530

    Add LinearSearchFindMin

---
+++ src/Notes/Searching/Linear/LinearSearchFindMin.java
@@ -0,0 +1,22 @@
+package Notes.Searching.Linear;
+
+public class LinearSearchFindMin {
+    public static void main(String[] args) {
+        int arr[] = {14, 53, 66, 32, 65, 2, 67};
+
+        if (arr.length != 0) {
+            System.out.println("Minimum: " + FindMin(arr));
+        }
+
+    }
+
+    static int FindMin(int arr[]) {
+        int min = arr[0];
+        for (int index = 0; index < arr.length; index++) {
+            if (min > arr[index]) {
+                min = arr[index];
+            }
+        }
+        return min;
+    }
+}
