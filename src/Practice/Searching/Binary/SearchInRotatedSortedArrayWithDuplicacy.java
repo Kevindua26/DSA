902be14e6bbf712cd0fe6f7cf9aa3bb179463b9c
Kevindua26 <kevindua26@gmail.com>
2024-12-30 20:34:26 +0530

    Add SearchInRotatedSortedArrayWithDuplicacy

---
+++ src/Practice/Searching/Binary/SearchInRotatedSortedArrayWithDuplicacy.java
@@ -0,0 +1,84 @@
+package Practice.Searching.Binary;
+
+public class SearchInRotatedSortedArrayWithDuplicacy {
+    public static void main(String[] args) {
+        int[] arr = {2,5,6,0,0,1,2};
+        int target = 0;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int search(int[] arr, int target) {
+        int pivot = findPivotInDuplicacy(arr);
+
+        if (pivot == -1) {
+            return binarySearch(arr, target, 0, arr.length - 1);
+        }
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+        if (arr[0] <= target) {
+            return binarySearch(arr, target, 0, pivot - 1);
+        } else {
+            return binarySearch(arr, target, pivot + 1, arr.length - 1);
+        }
+    }
+
+    static int findPivotInDuplicacy(int[] arr) {
+        // Here to find the pivot in duplicacy, we will ignore the duplicates
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (start < mid && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            // arr[] = {2, 9, 2, 2, 2}
+            if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                //check if start is pivot
+                if (arr[start] > arr[start + 1]){
+                    return start;
+                }
+                // else start will be incremented by 1
+                start++;
+
+                // same with the end
+                if (arr[end] < arr[end - 1]){
+                    return end - 1;
+                }
+                end--;
+            } else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target == arr[mid]) {
+                return mid;
+            }
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
