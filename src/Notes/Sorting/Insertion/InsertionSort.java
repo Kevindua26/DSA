b87c42812397ece17bdc9bb0d12570a9c9e2b1d7
Kevindua26 <kevindua26@gmail.com>
2025-02-17 02:04:10 +0530

    Add Insertion Sort

---
+++ src/Notes/Sorting/Insertion/InsertionSort.java
@@ -0,0 +1,29 @@
+package Notes.Sorting.Insertion;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {-7, 23, 67, 0, 4, -23};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr, int firstIndex, int secondIndex) {
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[secondIndex];
+        arr[secondIndex] = temp;
+    }
+}
