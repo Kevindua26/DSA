842734a15ed3cd3abfb58440a89c5c49858771ab
Kevindua26 <kevindua26@gmail.com>
2025-02-17 17:17:26 +0530

    Update Cyclic Sort

--- src/Notes/Sorting/Cycle/CyclicSort.java
+++ src/Notes/Sorting/Cycle/CyclicSort.java
@@ -5,16 +5,29 @@ import java.util.Arrays;
 public class CyclicSort {
     public static void main(String[] args) {
         int[] arr = {3, 5, 2, 1, 4};
-        sort(arr);
+//        sortOne(arr);
+        sortTwo(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void sort(int[] arr) {
+    static void sortOne(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int temp = arr[i];
             while (temp != i + 1) {
                 swap(arr, i, temp - 1);
                 temp = arr[i];
+            }
+        }
+    }
+
+    static void sortTwo(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
             }
         }
     }
